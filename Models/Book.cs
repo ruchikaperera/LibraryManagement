9f03c83b90a89631f7e1d9481b4322b5ed91af5d
Ruchika Perera <madubashitharuchika@yahoo.com>
2019-02-18 21:22:32 +0530

    Initial commit

---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string AuthorName { get; set; }
+        public int IsbnNo { get; set; }
+
+    }
+}
